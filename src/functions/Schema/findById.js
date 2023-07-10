e76303db00108d066289fdb3b051d2e52d9697d8
Ramco <ramazane791@gmail.com>
2023-07-10 13:00:52 +0300

    fix 3.0.12

--- src/functions/Schema/findById.js
+++ src/functions/Schema/findById.js
@@ -2,7 +2,7 @@ const { getData } = require("../get");
 
 function findById(name, id, db) {
   try {
-    return getData(`${id}`, `./${name}`);
+    return getData(`${id}`, `./${name}s.esos.db`);
   } catch (e) {
     return console.error(e.message);
   }
